47ff0946103e68a4ec56829c5ebda7d4f203eabc
Enzo Perego <enzorp@al.insper.edu.br>
2025-11-09 23:49:19 -0300

    Main com Auth0

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')).render(
   <Auth0Provider
-      domain="dev-vlbhbshl7b1pxe8e.us.auth0.com"
-      clientId="4rwe12yYJbxAD122EDkjl61unHBaHFL0"
+      domain="dev-c5cya7ea1phr4j8p.us.auth0.com"
+      clientId="qjbNYZujMhqauNgx8pgHhKvgIWliJWnP"
       authorizationParams={{
-        audience: "https://dev-vlbhbshl7b1pxe8e.us.auth0.com/api/v2/",
+        audience: "https://dev-c5cya7ea1phr4j8p.us.auth0.com/api/v2/",
         redirect_uri: window.location.origin
       }}
     >
